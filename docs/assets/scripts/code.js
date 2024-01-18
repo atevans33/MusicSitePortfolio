3d5d860632a56428928416bf885666fdc8380f34
Aaron Evans <154642006+atevans33@users.noreply.github.com>
2024-01-18 07:19:09 -0800

    Update code.js

--- docs/assets/scripts/code.js
+++ docs/assets/scripts/code.js
@@ -17,8 +17,6 @@ audio_index.pause();
 var m_breakpoint = 1024;//the breakpoint for mobile/tablet to desktop in px
 
 window.onload = function () {
-    document.getElementById('body-index').height = window.innerHeight;
-
     //flowtry page scripts
     var audio = document.getElementById("audio");
 
